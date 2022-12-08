1f993079efe90cf592c513b9c106459cc62b35d0
AlbuCristina92 <cristina.albu92@gmail.com>
2022-12-08 17:50:59 +0200

    cristina

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,33 @@
-import React from "react";
-import ImageShow from "./ImageShow";
+import React from 'react';
+import ImageShow from './ImageShow';
 
+class ImageList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const imagesList = this.props.images.map((image) => {
+      return <ImageShow key={image.id} image={image} />;
+    });
+    return (
+      <div
+        style={{
+          columns: '5 100px',
+          columnGap: '10px',
+        }}
+      >
+        {imagesList}
+      </div>
+    );
+  }
+}
+export default ImageList;
+/*
 const ImageList = (props) => {
   const imagesList = props.images.map((image) => {
     return <ImageShow key={image.id} image={image} />;
   });
-
   return (
     <div
       style={{
@@ -17,4 +39,4 @@ const ImageList = (props) => {
   );
 };
 
-export default ImageList;
+export default ImageList;*/
