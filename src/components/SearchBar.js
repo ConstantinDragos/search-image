f8c2eae3ff499852db2ac8b8afc6416125f7ffec
NiceHalex08 <alinbcd490@gmail.com>
2022-12-08 17:12:01 +0200

    Paduret

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,47 @@
-import React, { useState } from "react";
-import searchImages from "../helpers/searchHelper";
+import React, { Component, useState } from 'react';
+import searchImages from '../helpers/searchHelper';
 
-const SearchBar = ({ onSearchSubmit }) => {
-  const [search, setSearch] = useState("");
-
-  const handleChange = (e) => {
-    setSearch(e.target.value);
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { search: '' };
+    this.updateState = this.updateState.bind(this);
+  }
+  updateState = (e) => {
+    this.setState({ search: e.target.value });
   };
-
-  const onClick = async () => {
-    const images = await searchImages(search);
-    onSearchSubmit(images);
+  onClick = async () => {
+    const images = await searchImages(this.state.search);
+    this.props.onSearchSubmit(images);
   };
+  render() {
+    return (
+      <div className='search'>
+        <input value={this.state.search} onChange={this.updateState} />
+        <button onClick={this.onClick}>Search</button>
+      </div>
+    );
+  }
+}
 
-  return (
-    <div className="search">
-      <input value={search} onChange={handleChange} />
-      <button onClick={onClick}>Search</button>
-    </div>
-  );
-};
+// const SearchBar = ({ onSearchSubmit }) => {
+//   const [search, setSearch] = useState('');
+
+//   const handleChange = (e) => {
+//     setSearch(e.target.value);
+//   };
+
+//   const onClick = async () => {
+//     const images = await searchImages(search);
+//     onSearchSubmit(images);
+//   };
+
+//   return (
+//     <div className='search'>
+//       <input value={search} onChange={handleChange} />
+//       <button onClick={onClick}>Search</button>
+//     </div>
+//   );
+// };
 
 export default SearchBar;
